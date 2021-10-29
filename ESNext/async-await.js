72d37dedb42eb146128e4ecc400eb8492297e6b2
lobachaos <zwolfsasa@gmail.com>
2021-10-29 15:05:10 -0400

    Async Await

---
+++ ESNext/async-await.js
@@ -0,0 +1,27 @@
+const http = require("http");
+const getTurma = (letra, callback) => {
+    const url = `http://files.cod3r.com.br/curso-js/turma${letra}.json`
+    return new Promise((resolve, reject) => {
+        http.get(url, response => {
+            let resultado = ''
+            response.on('data', dados => {
+                resultado += dados
+            })
+            response.on('end', () => {
+                try {
+                    resolve(JSON.parse(resultado))
+                } catch (e) {
+                    reject(e)
+                }
+            })
+        })
+    })
+}
+
+let obterAlunos = async () => {
+    const turmaA = await getTurma('A')
+    const turmaB = await getTurma('B')
+    const turmaC = await getTurma('C')
+    return [].concat(turmaA,turmaB,turmaC)
+} // retorna um objeto AsyncFunction
+obterAlunos().then(alunos => alunos.map(a => a.nome)).then(i => console.log(i))
